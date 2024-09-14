431d4b090781f5aa35f764115902f36e52933bae
chaiyodcymg <chaiyod.cymg@gmail.com>
2024-09-14 22:12:35 +0700

    2_index_of_max

--- 2_index_of_max/main.py
+++ 2_index_of_max/main.py
@@ -20,4 +20,12 @@ output = list can not blank
 class Solution:
 
     def find_max_index(self, numbers: list) -> int | str:
-        pass
+        if len(numbers) == 0:
+            return "list can not blank"
+        max = 0
+        for index in range(len(numbers)):
+            if max == 0:
+                max = numbers[index]
+            if numbers[index] > max:
+                max = numbers[index]   
+        return max
