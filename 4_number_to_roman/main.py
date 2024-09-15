5083bb049ac0591ae6406f231926ca35c1559194
chaiyodcymg <chaiyod.cymg@gmail.com>
2024-09-15 13:08:26 +0700

    4_number_to_roman

--- 4_number_to_roman/main.py
+++ 4_number_to_roman/main.py
@@ -20,4 +20,24 @@ output = number can not less than 0
 class Solution:
 
     def number_to_roman(self, number: int) -> str:
-        pass
+        if number < 0:
+            return "number can not less than 0"
+        if number == 0 or number > 3999:
+            return "roman number rang from 1 to 3999"
+        roman_num = ""
+        digits_arr = [ 
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+        roman_arr = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+        for index in range(len(digits_arr)):
+            while number >= digits_arr[index]:
+                roman_num += roman_arr[index]
+                number -= digits_arr[index]
+        return roman_num
